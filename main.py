0b5e6aa2bec5f5c732dd0028dde3cafd2a384969
fmalato <federico.malato@stud.unifi.it>
2022-04-16 15:01:16 +0200

    Rotation modeled correctly

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     buffer_size = 150000
     lr = 1e-4
     total_timesteps = 1000000
-    train = False
-    save = False
-    on_linux = False
-    model_name = "DDPG_HER_5kk_mujoco"
+    train = True
+    save = True
+    on_linux = True
+    model_name = "DDPG_HER_1kk_mujoco"
     goal_selection_strategy = 'future'
     env = gym.make('geometry_mover:geometry_mover-v0', on_linux=on_linux)
     env = TimeLimit(env, max_episode_steps=max_episode_length)
@@ -36,20 +36,8 @@ if __name__ == '__main__':
                      ),
                      verbose=1)
     else:
-        """model = DDPG.load('saved_models/DDPG_HER_1kk_mujoco.zip',
-                          env=env)"""
-        model = DDPG(policy="MultiInputPolicy",
-                     env=env,
-                     replay_buffer_class=HerReplayBuffer,
-                     buffer_size=buffer_size,
-                     learning_rate=lr,
-                     replay_buffer_kwargs=dict(
-                         n_sampled_goal=num_sampled_goals,
-                         goal_selection_strategy=goal_selection_strategy,
-                         online_sampling=online_sampling,
-                         max_episode_length=max_episode_length,
-                     ),
-                     verbose=1)
+        model = DDPG.load('saved_models/DDPG_HER_1kk_mujoco.zip',
+                          env=env)
     if train:
         model.learn(total_timesteps=total_timesteps)
         fname = datetime.now().strftime("%H_%M_%S")
